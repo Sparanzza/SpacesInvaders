714480ee8a3d560c6d26f8635407b2b81cecea0a
Aurelio <aurelio@sparanzza.com>
2019-11-19 23:09:06 +0100

    Add look and feel SO

--- src/main/java/com/sparanzza/app/App.java
+++ src/main/java/com/sparanzza/app/App.java
@@ -2,6 +2,7 @@ package com.sparanzza.app;
 
 import com.sparanzza.ui.GameMainFrame;
 
+import javax.swing.*;
 import java.awt.*;
 
 /**
@@ -9,6 +10,18 @@ import java.awt.*;
  */
 public class App {
 	public static void main(String[] args) {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 		EventQueue.invokeLater(() -> {
 			new GameMainFrame();
 		});
